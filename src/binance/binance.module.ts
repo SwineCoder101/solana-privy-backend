2cd6bad97935bfe46265a10ee072bd030bd1b4d5
SwineCoder101 <liamfaruq@gmail.com>
2025-02-13 23:41:39 +0800

    enable-client-service-only

--- src/binance/binance.module.ts
+++ src/binance/binance.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { BinancePriceService } from './binance-price.service';
+import { BinanceClientService } from './binance-client.service';
 import { BinanceController } from './binance.controller';
-import { PriceAggregatorService } from './price-aggregator.service';
 
 @Module({
-  providers: [BinancePriceService, PriceAggregatorService],
+  providers: [BinanceClientService],
   controllers: [BinanceController],
-  exports: [BinancePriceService, PriceAggregatorService],
+  exports: [BinanceClientService],
 })
 export class BinanceModule {}
